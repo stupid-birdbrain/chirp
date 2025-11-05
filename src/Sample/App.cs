42ff2a2c44974d81446d45fd1b8e1b2449b95da3
mayli4 <sydsleeper@gmail.com>
2025-11-05 02:17:51 -0500

    init

---
+++ src/Sample/App.cs
@@ -0,0 +1,25 @@
+﻿using Miniaudio;
+using System.Runtime.InteropServices;
+
+namespace Sample;
+
+internal sealed unsafe class App {
+    public void Run(string[] args) {
+        ma_engine* engine = (ma_engine*)NativeMemory.Alloc((nuint)sizeof(ma_engine));
+        ma.engine_init(null, engine);
+
+        string filePath2 = "sound.wav";
+        ma_sound* sound2 = (ma_sound*)NativeMemory.Alloc((nuint)sizeof(ma_sound));
+        fixed (void* p = filePath2) {
+            ma.sound_init_from_file_w(engine, (ushort*)p, (uint)ma_sound_flags.MA_SOUND_FLAG_LOOPING, null, null, sound2);
+        }
+        ma.sound_start(sound2);
+
+        Console.ReadKey();
+        ma.sound_stop(sound2);
+
+        ma.engine_uninit(engine);
+        NativeMemory.Free(engine);
+        NativeMemory.Free(sound2);
+    }
+}
---
+++ src/Sample/Entry.cs
@@ -0,0 +1,8 @@
+﻿using Sample;
+
+file class Program {
+    static void Main(string[] args) {
+        var app = new App();
+        app.Run(args);
+    }
+}
